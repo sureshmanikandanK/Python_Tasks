2574ada237ce3dba71879955e74bfd1955833cd5
Sureshmanikandan K <sureshmanikandan.k>
2024-07-26 12:01:28 +0530

    CopyComp

---
+++ Task/Task_12_copy_comp_fie.py
@@ -0,0 +1,42 @@
+import os
+import filecmp
+
+def copy_file(file1, file2):
+    if not os.path.exists(file1):
+        print(f'File not exists: {file1}')
+    else:
+        file_1 = open(file1, 'r')
+        Friends = file_1.read()
+        file_1.close()
+        
+        file_2 = open(file2, 'w')
+        file_2.write(Friends)
+        file_2.close()
+        
+        print(f'Success -> Content copied from {file1} to {file2}')
+
+def compare_files(filename1, filename2):
+    if not os.path.exists(filename1):
+        print(f'Not Exist: {filename1}')
+    elif not os.path.exists(filename2):
+        print(f'Not Exist: {filename2}')
+    else:
+        compare = filecmp.cmp(filename1, filename2)
+        if compare:
+            print('Success -> The files are the same')
+        else:
+            print('Failure -> The files are different')
+
+def main():
+    print("Copy File Content:")
+    file1 = input('Enter the name of the file1: ')
+    file2 = input('Enter the name of the file2 to be created: ')
+    copy_file(file1, file2)
+    print()
+    print("Compare Files:")
+    file1_compare = input('Enter the name of the first file to compare: ')
+    file2_compare = input('Enter the name of the second file to compare: ')
+    compare_files(file1_compare, file2_compare)
+
+if __name__ == "__main__":
+    main()
---
+++ Task/sample2.py
@@ -0,0 +1,27 @@
+#open() function -> filename, mode(r,w,a...)
+
+# file_read = open('c:\Python\Task\Task\Task_3_calc.py','r')
+# print(file_read.read())
+
+import os
+def CreateFile(filename):
+    # filename = file_read.py
+    if(os.path.exists(filename)):
+        print('File alreay exists')
+    else:
+        file_create = open(filename,'w')
+
+def main():
+    print('Enter filename you want to create: ')
+    file_name = input()
+
+    CreateFile(file_name)
+
+if __name__=="__main__":
+    main()
+
+
+
+
+
+
